04cb678cceba1d3f9b0e7197155114408d64d1cd
Alberto Junior <alberto.junior.1995@gmail.com>
2018-10-30 10:11:29 -0300

    Gerar Cometas (#2)
    * Gerar Cometas
    
    * Pintando Cometas

---
+++ Assets/Scripts/CometBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CometBehaviour : MonoBehaviour {
+    public float velocity;
+    public Vector3 direction;
+
+    public float maxTimeActive;
+
+    private float actualyTime;
+	// Use this for initialization
+	void Start () {
+        if (maxTimeActive == 0)
+            maxTimeActive = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        actualyTime += Time.deltaTime;
+        if(actualyTime >= maxTimeActive)
+            Destroy(this.gameObject, 1f);
+
+        transform.Translate(direction * velocity * Time.deltaTime);
+    }
+}
